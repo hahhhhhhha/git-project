a1656d5fa186e0f5f3abc46d200e31fc61d8d696
hy <3466321523@qq.com>
2023-05-06 10:38:16 +0800

    master first push GitHub

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -6,5 +6,6 @@ public class App {
         System.out.println("hello git first modify");
         System.out.println("Hello git dl first modify11111");
         System.out.println("Hello git dl first modify22222");
+        System.out.println("Hello GitHub master first push GitHub");
     }
 }
