ca889dd58e5897f5e1a7264cd382d5b0d25460b8
hy <3466321523@qq.com>
2023-05-06 09:42:40 +0800

    first modify

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git first modify");
+        System.out.println("Hello git dl first modify");
     }
 }
